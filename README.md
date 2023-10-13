7490c3a48b12db60eaf7019dc668b89680c05f8c
dudushy <eduardotalarico1337@gmail.com>
2023-10-13 16:41:19 -0300

    Initial tweaks
    Yes, the UI is horrible

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![pages-build-deployment](https://github.com/dudushy/template-web/actions/workflows/pages/pages-build-deployment/badge.svg?branch=main)](https://github.com/dudushy/template-web/actions/workflows/pages/pages-build-deployment)
+[![pages-build-deployment](https://github.com/dudushy/cs2-cfg-generator/actions/workflows/pages/pages-build-deployment/badge.svg?branch=main)](https://github.com/dudushy/cs2-cfg-generator/actions/workflows/pages/pages-build-deployment)
 
-# [template-web](https://github.com/dudushy/template-web/)
-Template for Web projects.
+# [cs2-cfg-generator](https://github.com/dudushy/cs2-cfg-generator/)
+A simple tool that helps generate a Counter-Strike 2 CFG.
--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,51 @@
-const TITLE = "template-web";
+const TITLE = "cs2-cfg-generator";
 console.log(`[${TITLE}] init`);
+
+let selectedFiles = [];
+
+const fileInput = document.getElementById('vcfgInput');
+console.log(`[${TITLE}] fileInput`, fileInput);
+
+fileInput.onchange = () => {
+  console.log(`[${TITLE}#fileInput.onchange] (BEFORE) selectedFiles`, selectedFiles);
+
+  selectedFiles = [...fileInput.files];
+
+  console.log(`[${TITLE}#fileInput.onchange] (AFTER) selectedFiles`, selectedFiles);
+}
+
+const generateInput = document.getElementById('generateInput');
+console.log(`[${TITLE}] generateInput`, generateInput);
+
+generateInput.onclick = () => {
+  console.log(`[${TITLE}#generateInput.onclick]`);
+
+  generateCfg();
+}
+
+function generateCfg() {
+  for (const file of selectedFiles) {
+    console.log(`[${TITLE}#generateCfg] file`, file);
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const content = reader.result;
+      // const content = JSON.parse(reader.result);
+      console.log(`[${TITLE}#generateCfg#reader.onload] content`, content);
+
+      const cfg = 'test';
+      console.log(`[${TITLE}#generateCfg#reader.onload] cfg`, cfg);
+
+      const blob = new Blob([cfg], { type: 'text/plain' });
+      console.log(`[${TITLE}#generateCfg#reader.onload] blob`, blob);
+
+      const a = document.createElement('a');
+      a.download = `autoexec.cfg`;
+      a.href = URL.createObjectURL(blob);
+      a.click();
+    }
+
+    reader.readAsText(file);
+  }
+}
